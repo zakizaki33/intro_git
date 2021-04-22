d9060ba9d0ff0ca9be10c86488fb158086341f31
zakizaki33 <zakizaki33@hotmail.com>
2021-04-22 09:32:07 +0900

    initial commit

---
+++ test001.py
@@ -0,0 +1,9 @@
+# from  sympy import *
+print ("Hello World1 & Python !!!")
+
+print ("Hello World2 & Python !!!")
+
+
+print ("Hello World3 & Python !!!")
+
+print ("Hello World4 & 2011-04-22 !!!")
