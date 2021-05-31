80218a71c7deaa5c61be6e53f69b67f2871eaef8
zakizaki33 <zakizaki33@hotmail.com>
2021-05-31 10:05:20 +0900

    自動実行ファイルを追加しました。

---
+++ autorun.sh
@@ -0,0 +1,3 @@
+source c:/Users/13273_Yamazaki/OneDrive/Python/Git/.venv/Scripts/activate
+python test001.py
+python 20210324_1-video-GUI_0324.py
--- test001.py
+++ test001.py
@@ -3,4 +3,4 @@ import requests
 print("Hello World1 & Python !!!")
 print("Hello World4 & 2011-04-22 !!!")
 
-print(requests.get("https://www.python.jp").text)
+# print(requests.get("https://www.python.jp").text)
